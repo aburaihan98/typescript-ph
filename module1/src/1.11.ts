26566558c9dea3ede799207b8e626e6ca9f0b9d2
Abu Raihan <aburaihanrahmani@gmail.com>
2025-06-04 06:45:35 -0700

    ternary operator || optional chaining || nullish coalescing operator

---
+++ module1/src/1.11.ts
@@ -0,0 +1,41 @@
+{
+  // ternary operator || optional chaining || nullish coalescing operator
+  const age: number = 18;
+
+  if (age >= 18) {
+    console.log("adult");
+  } else {
+    console.log("not adult");
+  }
+
+  const isAdult = age >= 18 ? "adult" : "not adult";
+
+  const isAuthenticated = null;
+
+  const result1 = isAuthenticated ?? "Guest1";
+  const result2 = isAuthenticated ? isAuthenticated : "Guest2";
+  //   console.log({ result1 }, { result2 });
+
+  type User = {
+    name: string;
+    address: {
+      city: string;
+      road: string;
+      presentaddress: string;
+      permanentAddress?: string;
+    };
+  };
+
+  const user: User = {
+    name: "Persian",
+    address: {
+      city: "ctg",
+      road: "Awesome Road",
+      presentaddress: "ctg town",
+    },
+  };
+
+  const permanentAddress =
+    user?.address?.permanentAddress ?? "No Permanent Address";
+  console.log({ permanentAddress });
+}
