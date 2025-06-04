23653a2b532514c7f0147f35afebdcd4bd0f0935
Abu Raihan <aburaihanrahmani@gmail.com>
2025-06-04 07:24:38 -0700

    type assertion

---
+++ module1/src/1.4.js
@@ -0,0 +1,20 @@
+"use strict";
+// Basic Data Type
+// string
+let firstName = "Abu Raihan";
+// number
+let count = 1234;
+// boolean
+let isAdmin = true;
+// undefined
+let x = undefined;
+// null
+let y = null;
+let a;
+a = "a";
+console.log(a);
+// array
+let arrStr = ["a", "b", "c"];
+let arrNmb = [1, 2, 3];
+// tuple
+let arrAll = ["a", 1, true];
---
+++ module1/src/1.5.js
@@ -0,0 +1,8 @@
+"use strict";
+// object
+let obj = {
+    name: "Abu Raihan",
+    class: 10,
+    //   age: 26,
+    company: "PH",
+};
