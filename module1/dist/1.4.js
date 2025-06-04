2414cef8d22e336c94c48394dac2c2f380da5427
Abu Raihan <aburaihanrahmani@gmail.com>
2025-06-03 23:25:07 -0700

    tuple

--- module1/dist/1.4.js
+++ module1/dist/1.4.js
@@ -1,6 +1,15 @@
 "use strict";
+// Basic Data Type
+// string
 let firstName = "Abu Raihan";
+// number
 let count = 1234;
+// boolean
 let isAdmin = true;
+// undefined
 let x = undefined;
+// null
 let y = null;
+let a;
+a = "a";
+console.log(a);
--- module1/src/1.4.ts
+++ module1/src/1.4.ts
@@ -1,9 +1,28 @@
+// Basic Data Type
+// string
 let firstName: string = "Abu Raihan";
 
+// number
 let count: number = 1234;
 
+// boolean
 let isAdmin: boolean = true;
 
+// undefined
 let x: undefined = undefined;
 
+// null
 let y: null = null;
+
+let a: string;
+a = "a";
+console.log(a);
+
+// array
+let arrStr: string[] = ["a", "b", "c"];
+
+let arrNmb: number[] = [1, 2, 3];
+
+let arrAll: [string, number, boolean] = ["a", 1, true];
+
+// object
